bba18a3d0b398a05e9709973cb7ed5812f6655cc
mrene42 <mariarenebarco.969@gmail.com>
2025-02-05 02:46:26 +0100

    feat: add migration reservations

--- database/migrations/2025_02_05_010437_create_reservations_table.php
+++ database/migrations/2025_02_05_010437_create_reservations_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained("users")->onDelete('cascade');
+            $table->foreignId('flight_id')->constrained("flights")->onDelete('cascade');
             $table->timestamps();
         });
     }
