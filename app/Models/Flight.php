6f122b06f2db8899021219cc45c9dcaee2dcc0e3
mrene42 <mariarenebarco.969@gmail.com>
2025-03-24 17:29:00 +0100

    feat: add model flight

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Flight extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'departure',
+        'arrival',
+        'plane_id',
+        'available',
+    ];
+
+    public function plane(): BelongsTo
+    {
+        return $this->belongsTo(Plane::class);
+    }
 }
