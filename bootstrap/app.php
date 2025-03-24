12411db343c61d6405878d9e5044fbfe9fa61405
mrene42 <mariarenebarco.969@gmail.com>
2025-03-24 21:09:39 +0100

    feat: add middleware "IsAdmin", alias in app.php and JWT

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +14,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'Isadmin'=>IsAdmin::class,
+        ]);
+    })
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
