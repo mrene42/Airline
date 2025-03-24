0687f62cc9cdf20f14c4c00a093283ffc1e6d5ba
mrene42 <mariarenebarco.969@gmail.com>
2025-03-24 17:17:49 +0100

    feat: add column of imagen in plane table

--- database/migrations/2025_02_05_010341_create_planes_table.php
+++ database/migrations/2025_02_05_010341_create_planes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('seats');
-            $table->integer('max_capacity');
+            $table->string('imgplane');
             $table->timestamps();
         });
     }
