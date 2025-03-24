160651728f09714d49ddff9f2f68033322a4d888
mrene42 <mariarenebarco.969@gmail.com>
2025-03-24 17:34:22 +0100

    feat: add models Plane

--- app/Models/Plane.php
+++ app/Models/Plane.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Flight;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plane extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'seats',
+        'imgplane',
+    ];
+
+    public function flights(): HasMany
+    {
+        return $this->hasMany(Flight::class);
+    }
 }
