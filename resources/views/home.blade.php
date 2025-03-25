056fa9bf71d8a5f15d2bfac6b3c5a40484f8a684
mrene42 <mariarenebarco.969@gmail.com>
2025-03-25 12:53:27 +0100

    feat: add home blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Welcome to Airline</h1>
+    <p>Book your next flight with us!</p>
+    <p>Click <a href="{{ route('flightHome') }}">here</a> to view our flights.</p>
+
+@endsection
