9bfee272eda86d23c88523a48a3d17b80df21178
mrene42 <mariarenebarco.969@gmail.com>
2025-03-25 12:40:10 +0100

    feat: add header and footer blade

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
-</div>
+<footer class="bg-gray-800 text-white p-4 mt-10">
+    <div class="container mx-auto text-center">
+        <p>&copy; 2024 Airline. Todos los derechos reservados.</p>
+        <div class="flex justify-center space-x-4 mt-2">
+            <a href="#" class="hover:underline">Términos</a>
+            <a href="#" class="hover:underline">Privacidad</a>
+            <a href="#" class="hover:underline">Contacto</a>
+        </div>
+    </div>
+</footer>
